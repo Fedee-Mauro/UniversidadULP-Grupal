b178dae600b2864ab842b62c6540e1aa9af57553
Fedee-Mauro <federicolmauro@gmail.com>
2023-09-07 12:32:43 -0300

    Initial commit

---
+++ src/universidadulp/grupal/UniversidadULPGrupal.java
@@ -0,0 +1,10 @@
+package universidadulp.grupal;
+
+public class UniversidadULPGrupal {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
